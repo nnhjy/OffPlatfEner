5e9b3f282d292bf8edfbf2a55c760d0bbc38efe2
nnhjy <jiangyi.huang@outlook.com>
2023-06-15 01:59:42 +0200

    init commit

---
+++ Scripts/run_SpineOpt_customised.jl
@@ -0,0 +1,65 @@
+#=
+run_spineopt_customised:
+- Author: Huang, Jiangyi
+- Date: 2023-03-09
+=#
+
+using SpineOpt
+# using Gurobi
+using JuMP
+
+@time begin
+    m = run_spineopt(
+			ARGS...,
+			# mip_solver=optimizer_with_attributes(Gurobi.Optimizer),
+			# lp_solver=optimizer_with_attributes(Gurobi.Optimizer),
+			upgrade=true
+		)
+end
+
+# Show active variables and constraints
+println("*** Unlisted active values: ***")
+for key in setdiff(keys(m.ext[:spineopt].values), keys(m.ext[:spineopt].outputs))
+    !isempty(m.ext[:spineopt].values[key]) && println(key)
+end
+
+"""
+A function to print the active items in the built SpineOpt model.
+"""
+function print_active(m::JuMP.Model, item::Symbol)
+    println("*** Active $item: ***")
+    eval(
+        :(for key in keys(m.ext[:spineopt].$item)
+            !(isempty(m.ext[:spineopt].$item[key]) || isequal(m.ext[:spineopt].$item[key], (0, 0))) && println(key)
+        end)
+    )
+end
+
+items = [:variables, :constraints, :objective_terms]
+for i in items
+    print_active(m, i)
+end
+
+#=
+Alternatively, you can customize your run by using the form below.
+Keyword arguments are described in https://spine-tools.github.io/SpineOpt.jl/latest/library/#SpineOpt.run_spineopt
+
+m = run_spineopt(
+    ARGS...;
+    upgrade=false,
+    mip_solver=nothing,
+    lp_solver=nothing,
+    add_user_variables=m -> nothing,
+    add_constraints=m -> nothing,
+    update_constraints=m -> nothing,
+    log_level=3,
+    optimize=true,
+    update_names=false,
+    alternative="",
+    write_as_roll=0,
+    use_direct_model=false,
+    filters=Dict("tool" => "object_activity_control"),
+    log_file_path=nothing,
+    resume_file_path=nothing
+)
+=#
