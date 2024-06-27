6cd758a6d290082cc235fa88d829b6d72e84493e
nnhjy <jiangyi.huang@outlook.com>
2024-06-27 12:11:04 +0200

    update Euler setting

--- Scripts/Euler ETHz/run_SpineOpt.sh
+++ Scripts/Euler ETHz/run_SpineOpt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH -n 4 # 4 cores				# Guest users can use maximum 48 cores and 128G memory in total.
-#SBATCH --mem-per-cpu=8G # 8 GB per core	# Settings in a batch script only prescribes the resource for its own tasks. 
+#SBATCH -n 1 # 1 core				# Guest users can use maximum 48 cores and 128G memory in total.
+#SBATCH --mem-per-cpu=32G # 32 GB per core	# Settings in a batch script only prescribes the resource for its own tasks. 
 #SBATCH --time=4:00:00 # 4-hour run-time	# e.g. 4 cores * 16GB/core means we can simutaneously execute 2 of such scripts. 
 #SBATCH --tmp=10G # Local scratch space per node
 #SBATCH --job-name=runSpineOpt
@@ -9,7 +9,7 @@
 
 # to execute this script: "sbatch < run_SpineOpt.sh"
 
-module load gurobi gcc/11.4.0 julia/1.10.2
+module load gurobi julia/1.10.3
 
 cd $SCRATCH/path/to/the/SpineOptProject
 
